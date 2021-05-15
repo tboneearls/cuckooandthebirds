e47f402656f90a9a75ea98e951983b5f7bb154a2
Tyler Earls <tyler.a.earls@gmail.com>
2021-05-15 18:56:16 -0500

    first stab at navigation with submenu

--- components/navigation/NavBar.vue
+++ components/navigation/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="nav-container"
-    class="fixed z-10 flex items-center justify-end w-screen top-0 font-default text-white border border-gray-600 border-t-0 border-l-0 border-r-0 h-fit"
+    class="fixed z-10 flex items-center justify-end w-screen top-0 font-default text-white border border-gray-500 border-t-0 border-l-0 border-r-0 h-fit"
     :class="{'border-b-1 bg-coolgray-900': isNavActive, 'border-0 border-b-0': !isNavActive}"
   >
     <transition
@@ -17,30 +17,62 @@
           <li
             v-for="(section, index) in sections"
             :key="'section-' + index"
-            class="mx-auto py-2 text-center w-1/3 border border-t-0 border-l-0 border-r-0 border-b-1 border-gray-700 sm:border-none sm:mx-0 sm:w-auto"
-            :class="index === sections.length - 1 ? 'border-none' : ''"
+            class="flex items-center mx-auto pt-2 sm:py-2 text-center w-1/3 border border-t-0 border-l-0 border-r-0 border-b-1 border-gray-600 sm:border-none sm:mx-0 sm:w-auto"
+            :class="{'border-none': index === sections.length - 1, 'pb-2': (section.childLinks && section.childLinks.length === 0) || !section.showChildren}"
           >
             <router-link
+              v-if="section.childLinks && section.childLinks.length === 0"
               :to="section.href"
               :tabindex="isNavActive ? 0 : -1"
-              class="px-4 text-lg hover:text-cyan-300 hover:opacity-100 rounded-sm"
+              class="px-4 text-lg hover:text-cyan-300 hover:opacity-100 rounded-sm w-full sm:w-auto"
             >
               {{ section.name }}
             </router-link>
-            <!-- <ul v-if="section.childLinks && section.childLinks.length > 0">
-              <li 
-                v-for="(childLink, childLinkIndex) in sections.childLinks"
-                :key="'section-child-link-' + childLinkIndex"
-              >
+            <div 
+              v-else
+              class="w-full"
+            >
+              <div class="flex items-center">
                 <router-link
-                  :to="childLink.href"
+                  :to="section.href"
                   :tabindex="isNavActive ? 0 : -1"
-                  class="px-4 text-lg hover:text-cyan-300 hover:opacity-100 rounded-sm"
+                  class="pl-4 pr-2 text-lg hover:text-cyan-300 hover:opacity-100 rounded-sm w-full sm:w-auto"
                 >
-                  {{ childLink.name }}
+                  {{ section.name }}
                 </router-link>
-              </li>
-            </ul> -->
+                <button 
+                  v-if="section.childLinks && section.childLinks.length > 0"
+                  class="inline-block arrow-right mx-2"
+                  :class="{'down': section.showChildren}"
+                  @click="section.showChildren = !section.showChildren"
+                />
+              </div>
+              <div 
+                v-if="section.childLinks && section.childLinks.length > 0"
+                v-show="section.showChildren"
+              >
+                <ul class="static sm:absolute top-full z-20 flex flex-col sm:border sm:border-gray-600 bg-coolgray-900">
+                  <li 
+                    v-for="(childLink, childLinkIndex) in section.childLinks"
+                    :key="'section-child-link-' + childLinkIndex"
+                    class="inline-block text-center sm:text-left border-gray-700 w-full mx-auto sm:mx-0 sm:w-auto sm:border-gray-600"
+                    :class="{'sm:border-b': section.childLinks.length - 1}"
+                  >
+                    <hr 
+                      :class="{'my-2': childLinkIndex === 0}"
+                      class="sm:hidden border-gray-700 w-4/5 mx-auto"
+                    >
+                    <router-link
+                      :to="childLink.href"
+                      :tabindex="isNavActive ? 0 : -1"
+                      class="block px-4 my-2 text-sm sm:text-md hover:text-cyan-300 hover:opacity-100 rounded-sm w-full"
+                    >
+                      {{ childLink.name }}
+                    </router-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </li>
         </ul>
       </nav>
@@ -63,6 +95,7 @@ const sections = [
     href: "/",
     name: "Bio",
     childLinks: [],
+    showChildren: false,
   },
   {
     href: "/releases",
@@ -77,6 +110,7 @@ const sections = [
         name: "Show Me The Dark",
       },
     ],
+    showChildren: false,
   },
   // {
   //   href: "/shows",
@@ -87,11 +121,13 @@ const sections = [
     href: "/press",
     name: "Press",
     childLinks: [],
+    showChildren: false,
   },
   {
     href: "/booking",
     name: "Booking",
     childLinks: [],
+    showChildren: false,
   },
 ];
 
@@ -139,5 +175,15 @@ export default {
 #nav-container {
   will-change: border-bottom-width;
   transition: border-bottom-width 0.1s ease-in;
+}
+.arrow-right {
+  width: 0; 
+  height: 0; 
+  border-top: 6px solid transparent;
+  border-bottom: 6px solid transparent;
+  border-left: 6px solid white;
+}
+.arrow-right.down {
+  transform: rotate(90deg);
 }
 </style>
